058439454a352be046d9427dbeb671327ac3c775
kentaro_kami <kentaro_kami@kenduck.com>
2025-05-25 18:35:00 +0900

    batchをsubproject化

--- study/batch/retirement/src/main/java/com/kenduck10/study/batch/retirement/Application.java
+++ study/batch/retirement/src/main/java/com/kenduck10/study/batch/retirement/Application.java
@@ -1,13 +1,13 @@
-package com.kenduck10.study;
+package com.kenduck10.study.batch.retirement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AbcApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AbcApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- study/batch/retirement/src/test/java/com/kenduck10/study/batch/retire/ApplicationTests.java
+++ study/batch/retirement/src/test/java/com/kenduck10/study/batch/retire/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kenduck10.study;
+package com.kenduck10.study.batch.retirement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
