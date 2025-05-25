f4a4052ef02abedf6ce98196f7d6313d56a72265
kentaro_kami <kentaro_kami@kenduck.com>
2025-05-25 17:52:46 +0900

    batchスケルトン追加

---
+++ study/src/main/java/com/kenduck10/study/AbcApplication.java
@@ -0,0 +1,13 @@
+package com.kenduck10.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcApplication.class, args);
+	}
+
+}
